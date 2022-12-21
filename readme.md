e55a792603ac38207ed401aa9e643ecc5c4f078f
mhaprean <marianhaprean@gmail.com>
2022-12-21 18:41:23 +0200

    added upload images functionality

--- readme.md
+++ readme.md
@@ -2,6 +2,18 @@
 
 ### simple rest api starter with express, ts, and DETA.SH
 
-* register
-* login
-* visit private page
+- get started: 
+
+* create an account on https://www.deta.sh/ 
+* from your dashboard go to Project Keys and create a new Key
+* create an .env file simmilar to .env.local and update  your DETA credentials there
+
+
+#### features: 
+
+* register ( /api/auth/register )
+* login ( /api/auth/login )
+* visit private page ( /api/auth/profile)
+
+* upload images to DETA DRIVE. the file key should be "image"  ( /api/upload ). 
+* read the uploaded images ( /api/images/:imagename )
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+import upload, { UploadedFile } from 'express-fileupload';
+
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes';
@@ -22,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+app.use(upload());
 
 // add routes for our app
 app.use('/api/auth', authRoutes);
@@ -32,8 +35,35 @@ app.get('/', (req, res) => {
 
 const deta = Deta(process.env.DETA_PROJECT_KEY);
 
-const testDB = deta.Base('first_base');
+const drive = deta.Drive('images');
 
+app.post('/api/upload', async (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  } // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.image as UploadedFile;
 
+  const name = Date.now() + '_' + sampleFile.name;
+  const contents = sampleFile.data;
+
+  const img = await drive.put(name, { data: contents });
+  res.send(img);
+});
+
+app.get('/api/images/:name', async (req, res) => {
+  try {
+    const name = req.params.name;
+    const image = await drive.get(name);
+
+    if (image) {
+      const buffer = await image.arrayBuffer();
+      res.contentType('image/png').send(Buffer.from(buffer));
+    } else {
+      res.status(400).json('image doesnt exist');
+    }
+  } catch (error) {
+    return res.status(400).json('something went wrong');
+  }
+});
 
 export default app;
